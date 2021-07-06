b6ff71e4363d35dcefea4a361e11cf7cfd9ce321
Harini Dhanasekaran <63070913+harini-dhanasekaran@users.noreply.github.com>
2021-07-06 17:16:31 +0530

    changed reset mail Name

--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Haruk the god! <${process.env.EMAIL_FROM}>`;
+    this.from = `Haarsan Tours <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
